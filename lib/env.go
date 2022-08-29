2c354de8ea4c325e66e65605ee4a9677ff82efc1
Anh Phan <phanlong315@gmail.com>
2022-08-29 23:31:22 +0700

    ối dồi ôi

--- lib/env.go
+++ lib/env.go
@@ -3,16 +3,17 @@ package lib
 import (
 	"time"
 
+	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/viper"
 )
 
 type Env struct {
-	PORT string
+	PORT int
 
 	DB_USER string
 	DB_PASS string
 	DB_HOST string
-	DB_PORT string
+	DB_PORT int
 	DB_NAME string
 
 	JWT_EXPIRES_AT time.Duration
@@ -24,20 +25,19 @@ func NewEnv() (env Env) {
 	if err := viper.ReadInConfig(); err != nil {
 		panic(err)
 	}
+	viper.AutomaticEnv()
 
 	viper.SetDefault("PORT", 3000)
 
 	viper.SetDefault("DB_USER", "root")
 	viper.SetDefault("DB_PASS", "")
 	viper.SetDefault("DB_HOST", "localhost")
-	viper.SetDefault("DB_PORT", "3306")
+	viper.SetDefault("DB_PORT", 3306)
 	viper.SetDefault("DB_NAME", "test")
 
-	viper.AutomaticEnv()
+	viper.SetDefault("JWT_EXPIRES_AT", 86400)
+	viper.SetDefault("JWT_SECRET", []byte("ptanh97"))
 
-	env.JWT_EXPIRES_AT = viper.GetDuration("JWT_EXPIRES_AT")
-	env.JWT_SECRET = []byte(viper.GetString("JWT_SECRET"))
-
-	viper.Unmarshal(&env)
+	viper.Unmarshal(&env, viper.DecodeHook(mapstructure.ComposeDecodeHookFunc()))
 	return
 }
